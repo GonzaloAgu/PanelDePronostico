c19d580c98efb42dddef3a3f41508f4a5d9c1676
GonzaloAgu <gonzalo.ag88@gmail.com>
2022-03-22 17:44:13 -0300

    22032022

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const inputW = message => {
-    input = prompt(message);
-    document.write(input);
+
+const desc = message => {
+
+    let input = prompt(message);
+
+    // Verificación de input
+    while(input.length > 210){
+        const copia = input;
+        input = prompt('ERROR. Descripción demasiado larga.\n\n' + copia + '\n\n' + message)
+    }
+    
+    // Impresión de mensaje segun longitud
+    if(input.length < 70){ 
+        document.write(
+        `<p style="height: 16px;"></p>
+        <p>
+            ${input}
+        </p>`
+    )}
+    else if(input.length >= 70 && input.length < 140){
+        document.write(
+            `<p style="height: 8px;"></p>
+            <p>
+                ${input}
+            </p>`
+        )
+    }
+    else{
+        document.write(
+            `<p>
+                ${input}
+            </p>`
+        )
+    }
+
 }
 
 const icon = dia => {
@@ -27,4 +59,11 @@ const icon = dia => {
             `;
         document.write(result);
     }
+}
+
+const inputW = message => {
+    let input = prompt(message);
+
+    document.write(input);
+    if(input.length == 1){console.log('xd')};
 }
