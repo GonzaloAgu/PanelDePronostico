72995401e36fc89609c37102b0e1c60c856c105b
unknown <gonzalo.ag88@gmail.com>
2022-06-17 21:10:52 -0300

    función para agregar paneles

--- script.js
+++ script.js
@@ -73,5 +73,54 @@ const tempChange = (message, elemento) => {
 </h1>`
 }
 
+let contSu = 0;  //Contador de nuevos elementos superiores
+let contIn = 0;  //Contador de nuevos elementos inferiores
+const nuevoDia = pos => {
+    dia = prompt("Ingrese día a agregar");
+    /*Elijo contador segun input*/
+    if(pos == 'Ar'){
+        i = contSu;
+        contSu++;
+        console.log('Se añadirá un elemento superior ' + i);
+    }
+    else{
+        i = contIn;
+        contIn++;
+        console.log('Se añadirá el elemento inferior ' + i);
+    }
+
+    const diaBreve = dia.slice(0, 3);
+    const result = `<!--${dia.toUpperCase()}-->
+    <div id="cuerpo">
+    
+        <div id="header">
+            <p>${dia.toUpperCase()}</p>
+        </div>
+        <div class="tempMax" id="max${diaBreve}" onclick="tempChange('MAXIMA ${dia.toUpperCase()}', 'max${diaBreve}')"><h1>
+            Max
+        </h1></div>
+        <div class="tempMin" id="min${diaBreve}" onclick="tempChange('MINIMA ${dia.toUpperCase()}', 'min${diaBreve}')"><h1>
+            Min
+        </h1></div>
 
 
+        <div id="desc">
+            <div class="icons" id="${diaBreve}Icon" onclick="iconChange('${dia.toUpperCase()}', '${diaBreve}Icon');">
+                <div id="img2">
+                    <img src="D:/Documentos/PanoramaTemplate/icons/blank.png">
+                </div>
+            </div>
+    
+            <div class="info" id="${diaBreve}Info" onclick="descChange('${dia.toUpperCase()}', '${diaBreve}Info')">
+                <p style="height: 16px;"></p>
+                <p>
+                 Añadir descripción.
+                </p>
+            </div>
+        </div>
+    
+    </div>`;
+
+    document.getElementById(('nuevo' + pos + i)).innerHTML = result;
+    
+}
